aefaf9513ef34b6736576a1ac29885e5d869ac90
cc54321 <charles.cole@codeimmersives.com>
2023-05-12 23:32:13 -0400

    commit -m 'navbar'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+function NavBar() {
   return (
     <nav>
-      <h1>Recipe App</h1>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/search-form">Search Recipes</Link>
-      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/registration">Registration</Link>
+        </li>
+      </ul>
     </nav>
   );
-};
+}
 
 export default NavBar;
